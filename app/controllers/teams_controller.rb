fa0b30f84edb8c026723d626266d0d560fa52da4
Shmulik <syraices@gmail.com>
2019-08-23 05:33:34 +0000

    quizzes Two two

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -7,11 +7,12 @@ class TeamsController < ApplicationController
 
   def new
     @team = Team.new
-    redirect_to root_path
+    
   end
 
   def create
     @team = Team.create(team_params)
+    redirect_to root_path
   end
 
 
